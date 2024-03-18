32a8bc3ffecb372760b50ca38bd41206a622a66b
Mario De Luca <bloodcreed21@gmail.com>
2024-03-18 18:49:01 +0100

    Aggiornamenti al package.json e al manifest.json, aggiunta la funzionalità di aprire l'editor di frontmatter.

--- main.ts
+++ main.ts
@@ -16,66 +16,20 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: 'open-bulk-frontmatter-editor',
+			name: 'Open Frontmatter Editor',
+			callback: () => {
+				new FrontmatterEditorModal(this.app).open();
+				return true;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,23 +45,55 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class FrontmatterEditorModal extends Modal {
 	constructor(app: App) {
 		super(app);
+		this.extractFrontmatter();
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+
+	extractFrontmatter() {
+		const files = this.app.vault.getMarkdownFiles()
+
+		return files.map(file => {
+			const frontmatter = this.app.metadataCache.getFileCache(file)?.frontmatter
+			return { file: file.basename, frontmatter }
+
+		})
+	}
+
+	generateTemplate() {
+		const files = this.extractFrontmatter()
+		const template = files.map(file => {
+			return `
+			<div style='display:flex;align-items:center;margin-bottom:0.5rem'>
+				<input style='width:1.5rem;height:1.5rem' type="checkbox" id="${file.file}" name="${file.file}">
+				<label for="${file.file}">${file.file}</label>
+			</div>
+			`
+
+		})
+
+		return template.join('')
+	}
+
+	async onOpen() {
+		const { contentEl } = this;
+		contentEl.createEl('h2', { text: 'Frontmatter Editor' });
+		contentEl.createEl('p', { text: 'Select frontmatter to edit' });
+		contentEl.createEl('input', { type: 'text', attr: { placeholder: 'Search' } })
+
+		contentEl.createEl('p', { text: 'Select the files you want to edit' });
+		contentEl.createEl('div').innerHTML = this.generateTemplate()
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -116,7 +102,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
